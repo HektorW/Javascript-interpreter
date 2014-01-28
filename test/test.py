76a5b444b5528cc7912eda9063eef97622240562
Hektor Wallin <hektorw@gmail.com>
2014-01-28 23:13:46 +0100

    continued test cases but very little

--- test/test.py
+++ test/test.py
@@ -1,6 +1,19 @@
-from '../lexer' import lexer
+import imp
+
+token_definitions = imp.load_source('token_definitions', '../token_definitions.py')
+lexer = imp.load_source('lexer', '../lexer.py').lexer
 
 def testNumbers():
   l = lexer();
-  l.addToken('NUMBER', r'(?:[0-9]+)')
-  l.tokenize('1')
+  l.addToken('NUMBER', token_definitions.NUMBER)
+
+  def test(input, expected):
+    assert(l.tokenize(input)['NUMBER']['next'].group(0) == expected)
+
+  test('1', '1')
+  test('2', '2')
+
+  print('# All Number tests passed')
+
+
+testNumbers()
