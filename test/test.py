b83726f58551aae3b28323220d4ccdcd2c131ed9
Hektor Wallin <hektorw@gmail.com>
2014-01-30 23:12:00 +0100

    updated number definition and added more tests

--- test/test.py
+++ test/test.py
@@ -8,10 +8,12 @@ def testFailed(str):
   sys.exit()
 
 def equals(result, expected):
+  # assert(result == expected)
   if result != expected:
     testFailed('{} is not equal to {}'.format(result, expected))
 
 def notequals(result, expected):
+  # assert(result != expected)
   if result == expected:
     testFailed('{} should not equal {}'.format(result, expected))
 
@@ -25,12 +27,20 @@ def testNumbers():
   equals(lex.getTokenType('12312'), 'NUMBER')
   equals(lex.getTokenType('9128385123'), 'NUMBER')
   equals(lex.getTokenType('1.0'), 'NUMBER')
+  equals(lex.getTokenType('1.'), 'NUMBER')
+  equals(lex.getTokenType('.1'), 'NUMBER')
+  equals(lex.getTokenType('       1.1      '), 'NUMBER')
+  notequals(lex.getTokenType('NUMBER'), 'NUMBER')
+  # notequals(lex.getTokenType('1.1.'), 'NUMBER')
+  # notequals(lex.getTokenType('1.1.1'), 'NUMBER')
 
   # test value
   equals(lex.getTokenValue('1', 'NUMBER'), '1')
   equals(lex.getTokenValue('1.0', 'NUMBER'), '1.0')
   equals(lex.getTokenValue('1.', 'NUMBER'), '1.')
   equals(lex.getTokenValue('.1', 'NUMBER'), '.1')
+  equals(lex.getTokenValue('12345.0123', 'NUMBER'), '12345.0123')
+  equals(lex.getTokenValue('12345.0123', 'NUMBER'), '12345.0123')
 
 
   print('# All Number tests passed')
