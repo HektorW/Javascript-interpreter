2de2a995b852d8a18c572124e5e1bb6263494d2e
Hektor Wallin <hektorw@gmail.com>
2014-01-31 19:50:29 +0100

    defines all tokens in extern file

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from lexer import lexer
 import json
+import token_definitions
 
 str = 'x=1+2;'
 # batman = "Array(5).join(+{})+'\u0061 \u0042\u0061\u0074\u006d\u0061\u006e'"
 
 l = lexer()
 
-l.addToken('IDENTIFIER', r'[A-z\_\$]+[A-z0-9\_\$]*')
-l.addToken('NUMBER', r'(?:[0-9]+)') # only integers
-l.addToken('OPERATOR', r'[\+\*\-\/\=]')
-l.addToken('SEMICOLON', r';')
+l.addToken('IDENTIFIER', token_definitions.IDENTIFIER)
+l.addToken('NUMBER', token_definitions.NUMBER) # only integers
+l.addToken('OPERATOR', token_definitions.OPERATOR)
 
 tokens = l.parse(str)
 
